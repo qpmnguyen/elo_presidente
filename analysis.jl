b7bd8e71b027f3076bef0c81668b3307fe1b9565
Quang Nguyen <quangpmnguyen@gmail.com>
2020-11-07 10:49:42 -0500

    Adding Pkg.add for analysis.jl

--- analysis.jl
+++ analysis.jl
@@ -15,6 +15,15 @@ end
 
 # ╔═╡ a8f87970-209e-11eb-1039-75a77cfe4380
 begin 
+	import Pkg
+	Pkg.activate(mktempdir())
+	Pkg.add("DataFrames")
+	Pkg.add("Distributions")
+	Pkg.add("Query")
+	Pkg.add("CSV")
+	Pkg.add("Statistics")
+	Pkg.add("JSON")
+	Pkg.add("StatsFuns")
 	using DataFrames
 	using Distributions 
 	using Query
@@ -23,8 +32,7 @@ begin
 	using CSV
 	using Statistics
 	using JSON
-	include("elo.jl")
-	import Pkg
+	using StatsFuns
 	Pkg.add(url="https://github.com/Pocket-titan/DarkMode")
 	import DarkMode
 	DarkMode.enable()
@@ -35,39 +43,109 @@ md"
 # Predicting presidential election using weekly polls and the elo model
 "
 
+# ╔═╡ ab3fa37e-2110-11eb-2c23-715ad4d6fa2c
+md"Below are all the functions used"
+
+# ╔═╡ 56145ae0-2110-11eb-15b0-03b7d581b902
+begin
+	@doc raw"""
+	win_prob is a function to create win probabilities given an elo ranking   
+	r1::Int Elo rankings of player 1  
+	r2::Int Elo rankings of player 2   
+	Returns p1,p2 Tuple of win probabilities for player 1 and player 2  
+	"""
+	function win_prob(r1::Int, r2::Int)
+	    x1 = log(10)/400 * (r1 - r2)
+	    p1 = StatsFuns.logistic(x1)
+	    p2 = 1 - p1
+	    return(p1,p2)
+	end
+	
+	@doc raw"""
+	update_elo is a function that takes an elo and an outcome for the first player and returns updated elo (first player first)  
+	r1::Int Elo rankings of player 1  
+	r2::Int Elo rankings of player 2   
+	outcome::Int 0 or 1 indicating the win-loss status of player 1  
+	K::Int Scaling factor for elo   
+	"""
+	function update_elo(r1::Int, r2::Int, outcome::Int16, mov::Float64, K::Int=30)
+	    p1, p2 = win_prob(r1,r2)
+	    K = K * mov # just K as a proportion of margin of victory 
+	    r1new = convert(Int, round(r1 + K*(outcome - p1), digits = 0))
+	    r2new = convert(Int, round(r2 + K*((1 - outcome) - p2), digits = 0))
+	    return r1new, r2new
+	end
+	
+	@doc raw"""
+	elo_sequence is a function to get all elo sequences for a state given a results dictionary based on 538 data 
+	"""
+	function elo_sequence(outcome::Array, mov::Array, init::Tuple=(1000,1000), K::Int=30)
+	    if length(outcome) != length(mov)
+	        error("Length outcome has to be equal to mov...")
+	    end
+	    output = zeros(Int, length(outcome)+1, 2)
+	    (r1, r2) = init
+	    output[1,:] = [r1,r2]
+	    for i=1:length(outcome)
+	        r1,r2 = update_elo(r1,r2, outcome[i], mov[i], K)
+	        output[i+1,:] = [r1,r2]
+	    end
+	    return output
+	end
+	
+	function back_transformation(prob::Float64)
+	    logit(prob) * 400 * (1/log(10))
+	end
+	
+	# get elo sequence given poll data, K, state abbreviation and prior 
+	function get_elo(prior::DataFrame, polls::DataFrame, K::Int, abbv::Dict)
+	    states = unique(polls.state)
+	    result = DataFrame()
+	    for i=1:length(states)
+	        println(i)
+	        entry = polls |> @filter(_.state == states[i]) |> DataFrame
+	        pos_dates = unique(entry.dateweek)
+	        outcome = DataFrame(win = Int16[], mov = Float64[])
+	        for j=1:length(pos_dates)
+	            week_df = entry |> @filter(_.dateweek == pos_dates[j]) |> DataFrame
+	            max = maximum(week_df.pct_estimate_mean)
+	            mov =  abs(week_df.pct_estimate_mean[1] - week_df.pct_estimate_mean[2])/100
+	            idx = findall(x -> x == max, week_df.pct_estimate_mean)
+	            if (mov == 0)
+	                push!(outcome, [rand(Binomial(1,0.5)) rand(Uniform(0,0.01))])
+	            else
+	                push!(outcome, [ifelse(week_df.candidate_name[idx][1] == "Joseph R. Biden Jr.",1,0) mov])
+	            end
+	        end
+	        insertcols!(outcome,1, :poll_week => pos_dates)
+	        
+	        if states[i] in values(abbv)
+	            state_abb = [k for (k,v) in abbv if v == states[i]][1]
+	            insertcols!(outcome, 1, :state => vec(fill(state_abb, (1,length(outcome.win)))))
+	            init = Int.(round.(tuple(convert(Array, prior[prior.state .== state_abb,2:3] .+ 1000)...)))
+	            seq = elo_sequence(outcome.win, outcome.mov, init, K)
+	            insertcols!(outcome, 4, :biden_elo => seq[2:size(seq,1),1])
+	            insertcols!(outcome, 4, :trump_elo => seq[2:size(seq,1),2])
+	            result = vcat(result, outcome)
+	        end
+	    end
+	    return(result)
+	end
+	
+	# Simulating draws based on elo data set 
+	function simulation(result::DataFrame, nsim::Int, electoral_vote::Dict)
+	    output = DataFrame()
+	    probs = result.value
+	    for i=1:length(probs)
+	        outcome = rand(Bernoulli(probs[i]),nsim) .* electoral_vote[result.state[i]]
+	        insertcols!(output,1, Symbol(result.state[i]) => outcome, makeunique=true)
+	    end
+	    return(output)
+	end
+end
+
 # ╔═╡ 845465d0-209d-11eb-300e-355ff98531aa
 md"
-The elo rating system is a method developed by [Arpad Elo](https://en.wikipedia.org/wiki/Arpad_Elo) as a way to rank players in a zero sum game. Elo has been used in various contexts, particuarly notable in chess and more recently, competitive online games. A really nice attribute about Elo is that a difference in scores can be translated to a probability, through the formula:    
-
-$$E_A = \frac{1}{1 + 10^{R_A - R_B}}$$
-
-where $E_A$ is the expected score of player $A$ with rank $R_A$ playing against player $B$ of rank $R_B$. This is equivalent to applying the logistic function to a recaled version of the difference in rankings 
-
-$$logistic\left(\frac{\log(10)}{400} (R_A - R_B)\right)$$
-
-This ensures that the expected values $E_A$ and $E_B$ are bounded between 0 and 1 and hence acts as probabilities.  
-
-In an elo model, after each match, the rankings are updated as:  
-
-$$R_{A_{new}} = R_A + K * (\text{outcome} - E_A)$$   
-
-where $K$ is the $K$-factor controlling the relative gains and losses of elo points. 
-
-The US presidential election is the perfect place to test the elo theory since there is usually 2 candidates who campaigns for a long period of time with lots of polling data. The winner is determined through the electoral college, which follows a winner-take-all system. This further makes the US presidential election closer to a sports match.  
-
-In this instance, we decided to use the elo system to model the probability of winning an election. We take the weekly average of polls to act as \"games\" that then culiminate in a showdown on November 3rd. After each weekly poll, the candidate gains or lose elo based on the elo model presented above. Some **major caveats:**  
-
-* First, this is obviously not considering any fundamental of election forecasting, such as the inclusion of demographic and economical variables as well as state-by-state correlation.  
-
-* Second, this model does not incorporate any outside knowledge other than the way FiveThirtyEight weights the polls (we use average polling data from the site).  
-
-* Third, the model really preferences candidates who are polling well consistently, and rewards them with higher probability of winning. We think this is a pretty good model considering that close polling reduce the gains in elo and narrows the proability of winning, and sudden losses in polling numbers also contribute towards a candidate's poor showing. However, this is very non-traditional and not really an apt approach towards proper modelling of an election.   
-
-Considering those aspects, we further considered some modifications to the model. 
-* First, we seeded inital elo based on election results from the 2016 election. using 1000 elo as a baseline, we subtract or add points to candidate's starting elo based on how they (or the candidate belonging to their party) performed previously. We call this our priors.  
-* Second, we incorporate margin of victory into our elo update calculation by modifying the $K$-factor. This is a simple linear adjustment $K_{eff} = K * mov$ where $mov$ is the margin of victory. This means that the \"winning\" candidate will gain points as a proportion of K that is equal to their margin of polling victory. As such, we increase values of K significantly to compensate for the low gains in elo when margins can be 1 percent.  
-* Finally, we perform elo modelling at the state level, then use those probabilities to randomly draw a number of simulations based on a Bernoulli process. We calculate the final probability of winning as the number of simulations where a focal candidate (in this case Biden) gains 270 or more Electoral Votes.  
-
 This interactive Pluto.jl notebook also allows users to adjust the basic K factor, as well as viewing charts under different states/conditions. Since this is an interactive notebook, all values will be adjusted as sliders are changed. 
 "
 
@@ -159,7 +237,9 @@ perc_win = (sum(sim.win)/size(sim)[1])*100
 
 # ╔═╡ Cell order:
 # ╟─a3348840-207a-11eb-3d58-4ba80583bae5
-# ╟─a8f87970-209e-11eb-1039-75a77cfe4380
+# ╠═a8f87970-209e-11eb-1039-75a77cfe4380
+# ╟─ab3fa37e-2110-11eb-2c23-715ad4d6fa2c
+# ╟─56145ae0-2110-11eb-15b0-03b7d581b902
 # ╟─845465d0-209d-11eb-300e-355ff98531aa
 # ╟─0524d4b0-20a3-11eb-19f0-3bc4ca93563f
 # ╟─f1743320-2057-11eb-2d30-a92fc68ad63c
